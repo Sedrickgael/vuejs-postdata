21f40c6164e9c78e4fa3f5d26b9e0802b6b99b42
Sedrickgael <kanangamansedrickgael@gmail.com>
2019-10-11 17:56:41 +0000

    Initial commit

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for exovue project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exovue.settings')
+
+application = get_wsgi_application()
