15a018268aea4ea780f26d5bcf72dd1e932738e1
Sedrickgael <kanangamansedrickgael@gmail.com>
2019-10-13 13:11:11 +0000

    reglage

--- vuepost/datapost/views.py
+++ vuepost/datapost/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+from .models import *
 
 # Create your views here.
 
@@ -42,4 +43,35 @@ def senddata(request):
     }
     
     
+    return JsonResponse(datas, safe=False)
+
+
+def postimage(request):
+    nom = request.POST.get('nom')
+    username = request.POST.get('user')
+    password = request.POST.get('password')
+    passwordconf = request.POST.get('passwordconf')
+    email = request.POST.get('email')
+    phone = request.POST.get('phone')
+    
+    issucces = False
+    cont = 1
+    
+    if user is not None and nom is not None and password is not None and repass is not None and email is not None and contact is not None:
+        image = request.FILES['file']
+        issucces = True
+        h = Register(nom=nom,user=user,password=password,repass=repass,email=email,contact=contact,image=image)
+        h.save()
+        print(nom,user,contact,email,password,repass,image)
+    else:
+        issucces = False
+
+    while cont < 100000000:
+        cont += 1
+
+    datas = {
+        'succes': True,
+        'nom': nom,
+        'cont': cont
+    }
     return JsonResponse(datas, safe=False)
--- vuepost/vuepost/settings.py
+++ vuepost/vuepost/settings.py
@@ -31,13 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    'admin_interface',
+    'colorfield',   
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'datapost.apps.DatapostConfig'
+    'datapost.apps.DatapostConfig',
+    'django_admin_generator',
 ]
 
 MIDDLEWARE = [
